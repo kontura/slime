171b2c6e79a97a6c70c924f2ca7de89bd38ff301
Aleš Matěj <amatej@redhat.com>
2021-07-06 17:12:33 +0200

    Major refactor into functions

--- agents.cpp
+++ agents.cpp
@@ -10,7 +10,10 @@
 #include <cmath>
 #include <fstream>
 
-#define PARTICLES_COUNT 16*1024
+#define PARTICLES_COUNT 1024
+#define WIDTH 1920
+#define HEIGHT 1080
+
 
 std::string read_file(std::string path) {
 
@@ -51,26 +54,31 @@ bool check_program_link_status(GLuint obj) {
     return true;
 }
 
-int main() {
-    int width = 1920;
-    int height = 1080;
+void checkOpenGLErrors(const char *location) {
+    GLenum err = glGetError();
+    if (err != GL_NO_ERROR) {
+        fprintf(stderr, "OpenGL error from: %s: %s (%d)\n", location, gluErrorString(err), err);
+        exit(-1);
+    }
+}
 
+GLFWwindow *initializeOpenGl() {
     if(glfwInit() == GL_FALSE) {
         std::cerr << "failed to init GLFW" << std::endl;
-        return 1;
+        exit(11);
     }
 
     // select opengl version
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
     // create a window
     GLFWwindow *window;
-    if((window = glfwCreateWindow(width, height, "handmade_slime", 0, 0)) == 0) {
+    if((window = glfwCreateWindow(WIDTH, HEIGHT, "handmade_slime", 0, 0)) == 0) {
         std::cerr << "failed to open window" << std::endl;
         glfwTerminate();
-        return 1;
+        exit(11);
     }
 
     glfwMakeContextCurrent(window);
@@ -79,104 +87,166 @@ int main() {
         std::cerr << "failed to init GL3W" << std::endl;
         glfwDestroyWindow(window);
         glfwTerminate();
-        return 1;
+        exit(11);
     }
 
-    // shader source code
+    return window;
+}
+
+GLuint generateRenderProgram() {
+    GLuint program = glCreateProgram();
 
     // shader source code
     std::string vertex_source =
-        "#version 330\n"
+        "#version 430\n"
         "layout(location = 0) in vec4 vposition;\n"
-        "layout(location = 1) in vec4 vcolor;\n"
-        "out vec4 fcolor;\n"
+        "layout(location = 1) in vec2 vtexcoord;\n"
+        "out vec2 ftexcoord;\n"
         "void main() {\n"
-        "   fcolor = vcolor;\n"
+        "   ftexcoord = vtexcoord;\n"
         "   gl_Position = vposition;\n"
         "}\n";
 
     std::string fragment_source =
-        "#version 330\n"
+        "#version 430\n"
         "uniform sampler2D tex;\n" // texture uniform
         "in vec2 ftexcoord;\n"
-        "in vec4 fcolor;\n"
         "layout(location = 0) out vec4 FragColor;\n"
         "void main() {\n"
-        //"   FragColor = vec4(0,1,1,1);\n"
         "   FragColor = texture(tex, ftexcoord);\n"
         "}\n";
 
-    // program and shader handles
-    GLuint shader_program, vertex_shader, fragment_shader;
-
-    // we need these to properly pass the strings
-    const char *source;
-    int length;
-
     // create and compiler vertex shader
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    source = vertex_source.c_str();
-    length = vertex_source.size();
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    int length = vertex_source.size();
+    const char *source = vertex_source.c_str();
     glShaderSource(vertex_shader, 1, &source, &length);
     glCompileShader(vertex_shader);
     if(!check_shader_compile_status(vertex_shader)) {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return 1;
+        return 0;
     }
 
     // create and compiler fragment shader
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    source = fragment_source.c_str();
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     length = fragment_source.size();
+    source = fragment_source.c_str();
     glShaderSource(fragment_shader, 1, &source, &length);
     glCompileShader(fragment_shader);
     if(!check_shader_compile_status(fragment_shader)) {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return 1;
+        return 0;
     }
 
-    // create program
-    shader_program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
 
-    // attach shaders
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(program);
+    glUseProgram(program);
+    glUniform1i(glGetUniformLocation(program, "tex"), 0);
 
     // link the program and check for errors
-    glLinkProgram(shader_program);
-    check_program_link_status(shader_program);
+    check_program_link_status(program);
+
+    // data for a fullscreen quad (this time with texture coords)
+    GLfloat vertexData[] = {
+    //  X     Y     Z           U     V
+       1.0f, 1.0f, 0.0f,       1.0f, 1.0f, // vertex 0
+      -1.0f, 1.0f, 0.0f,       0.0f, 1.0f, // vertex 1
+       1.0f,-1.0f, 0.0f,       1.0f, 0.0f, // vertex 2
+      -1.0f,-1.0f, 0.0f,       0.0f, 0.0f, // vertex 3
+    }; // 4 vertices with 5 components (floats) each
+
+    GLuint indexData[] = {
+        0,1,2, // first triangle
+        2,1,3, // second triangle
+    };
+
+    GLuint vao, vbo, ibo;
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ibo);
+
+    // generate and bind the vertex buffer object
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    // fill with data
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*4*5, vertexData, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (char*)0 + 0*sizeof(GLfloat));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (char*)0 + 3*sizeof(GLfloat));
+
+    // generate and bind the index buffer object
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    // fill with data
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*2*3, indexData, GL_STATIC_DRAW);
+
+
+    checkOpenGLErrors("generateRenderProgram");
+
+    return program;
+}
+
+GLuint generateTexture() {
+    GLuint texture_handle;
+    glGenTextures(1, &texture_handle);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture_handle);
+
+    // set texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // set texture content
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, WIDTH, HEIGHT, 0, GL_RED, GL_FLOAT, NULL);
+    glBindImageTexture(0, texture_handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_R32F);
+
+    checkOpenGLErrors("generateTexture");
+
+    return texture_handle;
+}
+
+GLuint generateComputeProgram() {
+    GLuint program = glCreateProgram();
+    GLuint compute_shader = glCreateShader(GL_COMPUTE_SHADER);
+
+    std::string acceleration_source = read_file("../compute_shader");
+    // create and compiler vertex shader
+    const char *source = acceleration_source.c_str();
+    int length = acceleration_source.size();
+    glShaderSource(compute_shader, 1, &source, &length);
+    glCompileShader(compute_shader);
+    if(!check_shader_compile_status(compute_shader)) {
+        return 0;
+    }
+
+    // attach shaders
+    glAttachShader(program, compute_shader);
+
+    // link the program and check for errors
+    glLinkProgram(program);
+    check_program_link_status(program);
+
+    checkOpenGLErrors("generateComputeProgram");
+    return program;
+}
+
+int main() {
+    GLFWwindow *window = initializeOpenGl();
+
+    GLuint texture = generateTexture();
+    // program and shader handles
+    GLuint shader_program = generateRenderProgram();
 
     // get texture uniform location
     GLint texture_location = glGetUniformLocation(shader_program, "tex");
 
-    std::string acceleration_source = read_file("../compute_shader");
-
-    // program and shader handles
-    GLuint acceleration_program, acceleration_shader;
-
-    // create and compiler vertex shader
-    acceleration_shader = glCreateShader(GL_COMPUTE_SHADER);
-    source = acceleration_source.c_str();
-    length = acceleration_source.size();
-    glShaderSource(acceleration_shader, 1, &source, &length);
-    glCompileShader(acceleration_shader);
-    if(!check_shader_compile_status(acceleration_shader)) {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return 1;
-    }
 
     // create program
-    acceleration_program = glCreateProgram();
+    GLuint acceleration_program = generateComputeProgram();
 
-    // attach shaders
-    glAttachShader(acceleration_program, acceleration_shader);
-
-    // link the program and check for errors
-    glLinkProgram(acceleration_program);
-    check_program_link_status(acceleration_program);
 
     // the integrate shader does the second part of the euler integration
     std::string integrate_source =
@@ -215,8 +285,8 @@ int main() {
 
     // create and compiler vertex shader
     integrate_shader = glCreateShader(GL_COMPUTE_SHADER);
-    source = integrate_source.c_str();
-    length = integrate_source.size();
+    const char *source = integrate_source.c_str();
+    int length = integrate_source.size();
     glShaderSource(integrate_shader, 1, &source, &length);
     glCompileShader(integrate_shader);
     if(!check_shader_compile_status(integrate_shader)) {
@@ -236,35 +306,9 @@ int main() {
     check_program_link_status(integrate_program);
 
 
-    //TODO(amatej): why is this so slow? rather why it lags the pc when a tad larger?
-    //          ->> its because we iterate from every elem to every elem
-    //create textures where agents will draw and that will ultimately be displayed
-    // texture handle
-    GLuint texture;
-    // generate texture
-    glGenTextures(1, &texture);
-    // bind the texture
-    glBindTexture(GL_TEXTURE_2D, texture);
-    // create some image data
-    std::vector<GLubyte> image(4*width*height);
-    for(int j = 0;j<height;++j) {
-        for(int i = 0;i<width;++i) {
-            size_t index = j*width + i;
-            image[4*index + 0] = 0xFF*(j/10%2)*(i/10%2); // R
-            image[4*index + 1] = 0xFF*(j/13%2)*(i/13%2); // G
-            image[4*index + 2] = 0xFF*(j/17%2)*(i/17%2); // B
-            image[4*index + 3] = 0xFF;                   // A
-        }
-    }
-    // set texture parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    // set texture content
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &image[0]);
+    // CREATE INIT DATA
 
-    // randomly place particles in a cube
+    // randomly place agents
     float positionData[4*PARTICLES_COUNT];
     float velocityData[4*PARTICLES_COUNT];
     for(int i = 0;i<PARTICLES_COUNT;++i) {
@@ -273,40 +317,39 @@ int main() {
         positionData[index] = ((rand()%2000)-1000)/(float)2000;
         positionData[index+1] = ((rand()%2000)-1000)/(float)2000;
         positionData[index+2] = 0;
-        positionData[index+3] = 1;
+        positionData[index+3] = 0;
     }
 
-    // generate positions_vbos and vaos
-    GLuint vao, positions_vbo, velocities_vbo;
-
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    // generate positions_vbos and vaos and general vbo, ibo
+    GLuint positions_vbo, velocities_vbo;
 
     glGenBuffers(1, &positions_vbo);
     glGenBuffers(1, &velocities_vbo);
 
+    //ORIGINAL STUFF FOR AGENTS
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, velocities_vbo);
     glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(float)*4*PARTICLES_COUNT, velocityData, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, positions_vbo);
-
-    // fill with initial data
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4)*PARTICLES_COUNT, &positionData[0], GL_STATIC_DRAW);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*4*PARTICLES_COUNT, positionData, GL_STATIC_DRAW);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, positions_vbo);
+    glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(float)*4*PARTICLES_COUNT, positionData, GL_STATIC_DRAW);
 
     // set up generic attrib pointers
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (char*)0 + 0*sizeof(GLfloat));
+    //glEnableVertexAttribArray(0);
+    //glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (char*)0 + 0*sizeof(GLfloat));
 
+    //This is likely for compute shaders
     const GLuint ssbos[] = {positions_vbo, velocities_vbo};
     glBindBuffersBase(GL_SHADER_STORAGE_BUFFER, 0, 2, ssbos);
 
     // physical parameters
     float dt = 1.0f/60.0f;
 
+    //TODO(amatej): if I want to have the generateComputeProgram general (I need to pass in
+    //path to the compute shader it self) and also set uniforms outside?
     // setup uniforms
     glUseProgram(acceleration_program);
     glUniform1f(0, dt);
+    glUniform1i(glGetUniformLocation(acceleration_program, "destTest"), 0);
 
     glUseProgram(integrate_program);
     glUniform1f(0, dt);
@@ -343,25 +386,13 @@ int main() {
         // use the shader program
         glUseProgram(shader_program);
 
-        // bind texture to texture unit 0
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        
         // set texture uniform
         glUniform1i(texture_location, 0);
 
-        // bind the current vao
-        glBindVertexArray(vao);
-
         // draw
-        glDrawArrays(GL_POINTS, 0, PARTICLES_COUNT);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        // check for errors
-        GLenum error = glGetError();
-        if(error != GL_NO_ERROR) {
-            std::cerr << error << std::endl;
-            break;
-        }
+        checkOpenGLErrors("Main Loop");
 
         // finally swap buffers
         glfwSwapBuffers(window);
@@ -376,18 +407,18 @@ int main() {
     // delete the created objects
     glDeleteTextures(1, &texture);
 
-    glDeleteVertexArrays(1, &vao);
+    //glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &positions_vbo);
     glDeleteBuffers(1, &velocities_vbo);
 
-    glDetachShader(shader_program, vertex_shader);
-    glDetachShader(shader_program, fragment_shader);
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-    glDeleteProgram(shader_program);
+//    glDetachShader(shader_program, vertex_shader);
+//    glDetachShader(shader_program, fragment_shader);
+//    glDeleteShader(vertex_shader);
+//    glDeleteShader(fragment_shader);
+//    glDeleteProgram(shader_program);
 
-    glDetachShader(acceleration_program, acceleration_shader);
-    glDeleteShader(acceleration_shader);
+    //glDetachShader(acceleration_program, acceleration_shader);
+    //glDeleteShader(acceleration_shader);
     glDeleteProgram(acceleration_program);
 
     glfwDestroyWindow(window);
