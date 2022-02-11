4ca3ad8cd1a4ecea81535d1a07c4ab232a24c054
Aleš Matěj <amatej@redhat.com>
2022-02-11 21:12:07 +0100

    Change color

--- compute_shader.glsl
+++ compute_shader.glsl
@@ -60,7 +60,7 @@ void main() {
     int index = int(gl_GlobalInvocationID.x);
 
     vec2 center = vec2(float(width)/1.618f, float(height)/1.618f);
-    vec4 color = vec4(1, 0, 0, 1);
+    vec4 color = vec4(0, 0, 1, 1);
     Agent agent = agents[index];
 
     float agent_move_speed = moveSpeed;
