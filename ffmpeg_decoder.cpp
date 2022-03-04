be6d1036c7779800c6f32704e4b2fcdc2e6ded5a
Aleš Matěj <amatej@redhat.com>
2022-03-04 09:41:36 +0100

    Add empty ffmpeg modul (which compiles)

---
+++ ffmpeg_decoder.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+extern "C" {
+    #include <libavutil/frame.h>
+    #include <libavutil/mem.h>
+    #include <libavcodec/avcodec.h>
+}
+
+int loadSong(const char *path) {
+    return 0;
+}
+
---
+++ ffmpeg_decoder.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+int loadSong(const char* path);
