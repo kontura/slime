c5519c8424efb2bb42351ec20a824c7692e7aeb0
Aleš Matěj <amatej@redhat.com>
2022-05-22 07:47:25 +0200

    [slime] adjust ranges for max computing

--- src/slime.cpp
+++ src/slime.cpp
@@ -74,20 +74,20 @@ void run_slime(Slime * slime, fftw_complex * out_complex_l, float dt,
     float float_max, float_max_type2;
     double max_low = 0;
     double max_high = 0;
-    for (int i=0;i<512;i++) {
+    for (int i=0;i<212;i++) {
         if (max_low < hypot(out_complex_l[i][0], out_complex_l[i][1])) {
             max_low = hypot(out_complex_l[i][0], out_complex_l[i][1]);
         }
         //printf("%d", hypot(out_complex_l[i][0], out_complex_l[i][1]));
     }
-    for (int i=512;i<(FFTW_SAMPLES+1);i++) {
+    for (int i=212;i<(FFTW_SAMPLES+1);i++) {
         if (max_high < hypot(out_complex_l[i][0], out_complex_l[i][1])) {
             max_high = hypot(out_complex_l[i][0], out_complex_l[i][1]);
         }
         //printf("%d", hypot(out_complex_l[i][0], out_complex_l[i][1]));
     }
     float_max = (float)max_low / (float)51200000* 7;
-    float_max_type2 = (float)max_high / (float)51200000* 7;
+    float_max_type2 = (float)max_high / (float)5120000* 7;
 
     glUseProgram(slime->acceleration_program);
     glUniform1f(glGetUniformLocation(slime->acceleration_program, "moveSpeed"), 0.3f + float_max*MOVE_SPEED);
